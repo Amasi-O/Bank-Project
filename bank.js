73e94a9997bfca208950424fa9274a919c976215
Amasi-O <101877015+Amasi-O@users.noreply.github.com>
2022-05-19 14:28:14 +0100

    Delete bank.js

--- bank.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import Bank from './build/Bank.json';
-
-const instance = new web3.eth.Contract(
-    Bank.Bank.abi,
-    '0x8715385942e8dE8A694478dDeF75D565Eb8fACA8'
-);
-
-export default instance;
